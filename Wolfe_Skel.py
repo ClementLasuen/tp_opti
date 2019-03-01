8b17a4cfc3b57592683606c90b50e85571be1a7c
ClementLasuen <36895029+ClementLasuen@users.noreply.github.com>
2019-03-01 18:28:17 +0100

    Update Wolfe_Skel.py

--- Wolfe_Skel.py
+++ Wolfe_Skel.py
@@ -119,26 +119,26 @@ def Gradient_V(Oracle,x0):
     print("End boucle")
     return Oracle(x)[0],Oracle(x)[1],x,compteur
 
-def polak(x0,OraclePG):
+def Polak_Ribiere(x0,Oracle):
     
     
     epsilon = 0.0001
     k = 1
-    g_k_1 = OraclePG(x0,3) # gradient à l'étape k-1
-    g_k = OraclePG(x0,3) # gradient à l'étape k
-    d_k = -gradient # direction de descente à l'étape k
+    g_k_1 = Oracle(x0,3) # gradient à l'étape k-1
+    g_k = Oracle(x0,3) # gradient à l'étape k
+    d_k = -g_k # direction de descente à l'étape k
     
     beta = 1
     alpha = 1
     
     x = x0 + d_k
     
-    while np.linalg.norm(gradient) >= epsilon and k < 1000 :
+    while np.linalg.norm(g_k) >= epsilon and k < 1000 :
         
         # gradient à l'etape k
-        g_k = OraclePG(x,3)
+        g_k = Oracle(x,3)
             
-        alpha = Wolfe(alpha, x ,  gradient  ,OraclePG)[0]
+        alpha = Wolfe(alpha, x ,  -g_k  ,Oracle)[0]
         beta = np.dot(g_k, g_k - g_k_1)/np.linalg.norm(g_k_1)**2
         
                 
@@ -146,23 +146,74 @@ def polak(x0,OraclePG):
         
         x += beta*d_k # x_k+1
 
-        gradient_k_1 = gradient_k
+        g_k_1 = g_k
     
-    return x,OraclePG(x,3),gradient_k
+    return Oracle(x)[0],Oracle(x)[1],x,k
 
-# def BFGS(x0,OraclePG):
-#     
-#     epsilon = 0.0001
-#     k = 0
-#     gradient_k_1 = OraclePG(x0,3) # gradient à l'étape k-1
-#     gradient_k = OraclePG(x0,3) # gradient à l'étape k
-#     d_k = -gradient # direction de descente à l'étape k
-#     
-#     beta = 1
-#     alpha = 1
-#     
-#     x = x0 + d_k
-#     delta_x = x
-#     delta_g = 
-#     while np.linalg.norm(gradient) >= epsilon and k < 1000 :
-#         
+ def BFGS(x0,Oracle):
+     
+     epsilon = 0.0001
+     k = 0
+     g_k_1 = Oracle(x0,3) # gradient à l'étape k-1
+     
+
+     
+     beta = 1
+     alpha = 1
+     
+     n = d_k.shape[0]
+     W = np.eye(n)
+     
+     x_k = x0 -g_k_1
+     x_k_1 = x0
+     delta_x = x_k
+     delta_g = g_k
+     
+     g_k = Oracle(x_k,3) # gradient à l'étape k
+     
+     while np.linalg.norm(g_k) >= epsilon and k < 10 :
+         
+         g_k = Oracle(x_k,3)
+         
+         # J'arrive pas à calculer la MATRICE delta_x ^T * delta_g
+         # dot calcule toujours le produit scalaire
+         delta_x = x_k- x_k_1
+         delta_g = g_k - g_k_1
+         A_xg = W
+         A_gx = W
+         A_xx = W
+         for i in range(n):
+             for j in range(n):
+                 A_xg[i,j] = delta_x[i]*delta_g[j] /(delta_x @ delta_g)
+                 A_xg[i,j] = delta_g[i]*delta_x[j] /(delta_x @ delta_g)
+                 A_xg[i,j] = delta_x[i]*delta_x[j] /(delta_x @ delta_g)
+                 
+         W = (np.eye(n)- A_xg) @ W @ ((np.eye(n)- A_gx)) + A_xx
+         alpha = Wolfe(alpha, x ,  -g_k  ,Oracle)[0]
+         
+         x_k_1 = x_k
+         x_k -= alpha* (W @ g_k)
+         
+         k+=1
+    return Oracle(x_k)[0],Oracle(x_k)[1],x_k,k
+
+def Newton(x0,Oracle):
+    
+    epsilon = 0.0001
+    k=0
+    x = x0
+    
+    G, H = Oracle(x0,6)
+    
+    d = np.linalg.solve(H,G)
+    alpha =1
+    
+    while np.linalg.norm(G) >= epsilon and k < 10 and np.linalg.det(H) != 0 :
+        
+        d = np.linalg.solve(H,G)
+        k+=1
+        alpha = Wolfe(alpha, x ,  d  ,Oracle)[0]
+        x += alpha*d
+        G, H = Oracle(x,6)
+        
+    return Oracle(x)[0],G,x,k
